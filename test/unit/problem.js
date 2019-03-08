9c2bccf29b2e8965e620a81c254e1e4ad35bf80e
Steve Ripberger <sripberger@batterii.com>
2019-03-08 15:22:18 -0500

    Extract Problem#_addKeySet

--- test/unit/problem.js
+++ test/unit/problem.js
@@ -25,6 +25,7 @@ describe('Problem', function() {
 		beforeEach(function() {
 			keySet = new keySetModule.KeySet();
 			sinon.stub(keySetModule, 'KeySet').returns(keySet);
+			sinon.stub(problem, '_addKeySet');
 			sinon.stub(problem, '_validateKeys');
 		});
 
@@ -36,68 +37,15 @@ describe('Problem', function() {
 			expect(keySetModule.KeySet).to.be.calledWith('foo', 'bar');
 		});
 
-		it('adds an item for each key set value', function() {
-			problem.items = { foo: {} };
-			keySet.values.push('bar', 'baz');
-
+		it('adds the key set to the problem', function() {
 			problem.add();
 
-			expect(problem.items).to.have.keys('foo', 'bar', 'baz');
+			expect(problem._addKeySet).to.be.calledOnce;
+			expect(problem._addKeySet).to.be.calledOn(problem);
+			expect(problem._addKeySet).to.be.calledWith(keySet);
 		});
 
-		it('copies key set before and after property to items', function() {
-			keySet.values.push('foo');
-			keySet.before = [ 'bar', 'baz' ];
-			keySet.after = [ 'omg', 'wow' ];
-
-			problem.add();
-
-			expect(problem.items.foo).to.deep.equal({
-				before: keySet.before,
-				after: keySet.after,
-			});
-		});
-
-		it('skips empty before property', function() {
-			keySet.values.push('foo');
-			keySet.after = [ 'bar' ];
-
-			problem.add();
-
-			expect(problem.items.foo).to.deep.equal({ after: keySet.after });
-		});
-
-		it('skips empty after property', function() {
-			keySet.values.push('foo');
-			keySet.before = [ 'bar' ];
-
-			problem.add();
-
-			expect(problem.items.foo).to.deep.equal({ before: keySet.before });
-		});
-
-		it('adds a group with values, if key set has one', function() {
-			keySet.values.push('foo', 'bar', 'baz');
-			keySet.group = 'qux';
-
-			problem.add();
-
-			expect(problem.groups).to.have.keys('qux');
-			expect(problem.groups.qux).to.deep.equal([ 'foo', 'bar', 'baz' ]);
-		});
-
-		it('appends to group values, if they already exist', function() {
-			keySet.values.push('foo', 'bar');
-			keySet.group = 'baz';
-			problem.groups.baz = [ 'qux' ];
-
-			problem.add();
-
-			expect(problem.groups).to.have.keys('baz');
-			expect(problem.groups.baz).to.deep.equal([ 'qux', 'foo', 'bar' ]);
-		});
-
-		it('invokes #_validateKeys with values and group', function() {
+		it('invokes #_validateKeys with key set values and group', function() {
 			keySet.values.push('foo', 'bar');
 			keySet.group = 'baz';
 
@@ -111,16 +59,80 @@ describe('Problem', function() {
 			);
 		});
 
-		it('does not change instance if #_validateKeys throws', function() {
-			problem.items = { foo: {} };
-			problem.groups = { bar: [ 'foo' ] };
-			keySet.values.push('baz');
-			keySet.group = 'qux';
+		it('does not add the key set if #_validateKeys throws', function() {
 			problem._validateKeys.throws(new Error('omg'));
 
 			expect(() => problem.add()).to.throw();
-			expect(problem.items).to.deep.equal({ foo: {} });
-			expect(problem.groups).to.deep.equal({ bar: [ 'foo' ] });
+			expect(problem._addKeySet).to.not.be.called;
+		});
+	});
+
+	describe('#_addKeySet', function() {
+		let keySet;
+
+		beforeEach(function() {
+			keySet = new keySetModule.KeySet();
+		});
+
+		it('adds an item for each key set value', function() {
+			problem.items = { foo: {} };
+			keySet.values.push('bar', 'baz');
+
+			problem._addKeySet(keySet);
+
+			expect(problem.items).to.have.keys('foo', 'bar', 'baz');
+		});
+
+		it('copies key set before and after property to items', function() {
+			keySet.values.push('foo');
+			keySet.before = [ 'bar', 'baz' ];
+			keySet.after = [ 'omg', 'wow' ];
+
+			problem._addKeySet(keySet);
+
+			expect(problem.items.foo).to.deep.equal({
+				before: keySet.before,
+				after: keySet.after,
+			});
+		});
+
+		it('skips empty before property', function() {
+			keySet.values.push('foo');
+			keySet.after = [ 'bar' ];
+
+			problem._addKeySet(keySet);
+
+			expect(problem.items.foo).to.deep.equal({ after: keySet.after });
+		});
+
+		it('skips empty after property', function() {
+			keySet.values.push('foo');
+			keySet.before = [ 'bar' ];
+
+			problem._addKeySet(keySet);
+
+			expect(problem.items.foo).to.deep.equal({ before: keySet.before });
+		});
+
+		it('adds a group with values, if key set has one', function() {
+			keySet.values.push('foo', 'bar', 'baz');
+			keySet.group = 'qux';
+
+			problem._addKeySet(keySet);
+
+			expect(problem.groups).to.have.keys('qux');
+			expect(problem.groups.qux).to.deep.equal([ 'foo', 'bar', 'baz' ]);
+		});
+
+		it('appends to group values, if they already exist', function() {
+			keySet.values.push('foo', 'bar');
+			keySet.group = 'baz';
+			problem.groups.baz = [ 'qux' ];
+
+			problem._addKeySet(keySet);
+
+			expect(problem.groups).to.have.keys('baz');
+			expect(problem.groups.baz).to.deep.equal([ 'qux', 'foo', 'bar' ]);
 		});
 	});
 
